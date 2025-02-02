6bcb96ee7b12d398e0c5200f45c7c082b342523e
viniciusteixeiradias <viniciustdias2001@gmail.com>
2025-02-02 19:26:23 +1000

    page 21 ebook

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 123456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	SLASH    = "/"
+	ASTERISK = "*"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
